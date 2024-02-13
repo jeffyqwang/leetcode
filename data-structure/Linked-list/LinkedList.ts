9e8c1055dbb116ff3532b8b3fabeb82c34a4c35d
jeffyqwang <jeffyqwang@tencent.com>
2024-02-13 10:18:49 +0800

    feat: add LInkedList type definition

---
+++ data-structure/Linked-list/LinkedList.ts
@@ -0,0 +1,11 @@
+export class LinkedListNode {
+  value: any;
+  next: LinkedListNode | null;
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+export interface ILinkedListNode {
+  value: any,
+  next: ILinkedListNode | null}
