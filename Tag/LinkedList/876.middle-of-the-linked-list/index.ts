4335818290a6f5f724b3cb3403ffbe41ff03ad8c
jeffyqwang <657121430@qq.com>
2025-02-22 20:59:42 +0800

    feat:  leetcode 876 FastSlowPointersSolution

---
+++ Tag/LinkedList/876.middle-of-the-linked-list/index.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=typescript
+ * @lcpr version=30204
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+  let slow = head,
+    fast = head;
+  while (fast?.next) {
+    fast = fast.next.next;
+    slow = slow!.next;
+  }
+  return slow;
+}
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,6]\n
+// @lcpr case=end
+
+ */
