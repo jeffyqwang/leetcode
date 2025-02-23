05c769537c6a7a06e723fbe541c7fa83424705a0
jeffyqwang <657121430@qq.com>
2025-02-23 16:46:20 +0800

    feat: add memoized recursion solution for leetcode 70 Climbing Stairs

---
+++ Tag/Recursion/70.climbing-stairs/index.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ * @lcpr version=30204
+ *
+ * [70] 爬楼梯
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+const hasSolvedList = new Map<number, number>();
+function climbStairs(n: number): number {
+  //递推公式，第n阶楼梯有两种走法，一步一阶和一步两阶，所以f(n) = f(n-1)+f(n-2)
+  //终止条件为f(1) = 1, f(2) = 2;
+  //实际上为斐波那契数列
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+  if (hasSolvedList.has(n)) return hasSolvedList.get(n)!;
+  const res = climbStairs(n - 1) + climbStairs(n - 2);
+  hasSolvedList.set(n, res);
+  return res;
+}
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+ */
